f9d91c3a3c8f0aac8b8c7db4e76c1c5ebfbf03a7
venkatesh-subramani <venkateshs71097@gmail.com>
2023-01-18 19:18:34 +0530

    Added application.properties file to configure MySql database.

--- src/main/java/com/venkat/Repository/ClientRepository.java
+++ src/main/java/com/venkat/Repository/ClientRepository.java
@@ -2,9 +2,11 @@ package com.venkat.Repository;
 
 import com.venkat.Model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
     Optional<Client> findById(Long id);
 
